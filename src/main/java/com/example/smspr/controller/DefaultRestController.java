e993ba0d4881eae79620a7e738438fc81a4c086a
Jangmyun <cndlsrb2739@gmail.com>
2024-07-09 15:10:42 +0900

    create DefaultRestController

--- src/main/java/com/example/smspr/controller/DefaultRestController.java
+++ src/main/java/com/example/smspr/controller/DefaultRestController.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
 
-@RequestMapping("")
+
+@RequestMapping("/api")
 @RestController
 public class DefaultRestController {
 
@@ -19,5 +22,27 @@ public class DefaultRestController {
     @GetMapping("/add")
     public  String add(@RequestParam int test1, int test2){
         return (test1 + test2) +"" ;
+    }
+
+    @GetMapping("/login")
+    public Map<String, Object> login(@RequestParam Map<String,Object> params){
+        boolean result = false;
+        String msg = "";
+        if("admin".equals(params.get("id"))){
+            if("abcd".equals(params.get("pw"))){
+                result = true;
+                msg = "정상 로그인";
+            }else {
+                msg = "Wrong PW";
+            }
+        }else {
+            msg = "Wrong ID";
+        }
+
+        Map<String, Object> returnData = new HashMap<>();
+        returnData.put("result", result);
+        returnData.put("msg",msg);
+
+        return returnData;
     }
 }
